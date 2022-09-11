7cd106cd551cf4fc9791799bb20ca17e052ebbfb
amstertuk <74322785+amstertuk@users.noreply.github.com>
2022-09-11 13:24:31 +0300

    Update ex_2.java

--- pr_2/ex_2.java
+++ pr_2/ex_2.java
@@ -4,3 +4,22 @@
 2. Метод обчислює їх суму та повертає значення.
 3. Викликати цей метод методу main.
 4. Зробити методи для обчислення різниці, твору та поділу.
+
+package main;
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    
+    
+    System.out.println(a + b);
+    System.out.println(a - b);
+    System.out.println(a * b);
+    System.out.println(a / b);
+    
+	}
+
+}
